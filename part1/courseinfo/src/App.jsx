76b61f934a4889a8043defe17c3b298290360381
Manuela Schneider <manuelagottschalck1103@gmail.com>
2026-07-16 22:40:58 -0300

    Adding 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,24 +4,26 @@ import Total from './Total.jsx';
 
 const App = () => {
   const course = 'Desenvolvimento de aplicação Half Stack'
-  const part1 = {
-    name: 'Fundamentos da biblioteca React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Usando props para passar dados',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'Estado de um componente',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentos da biblioteca React',
+      exercises: 10
+    },
+    {
+      name: 'Usando props para passar dados',
+      exercises: 7
+    },
+    {
+      name: 'Estado de um componente',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content props={[part1, part2, part3]} />
-      <Total numbers={[part1.exercises, part2.exercises, part3.exercises]} />
+      <Content props={parts} />
+      <Total numbers={parts.map(x => x.exercises)} />
     </div>
   )
 }
